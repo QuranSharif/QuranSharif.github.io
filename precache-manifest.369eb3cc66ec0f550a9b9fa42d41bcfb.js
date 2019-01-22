4a63015756bc5852be31076a0a3ef29bb4ff688f
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-01-22 18:19:21 +0530

    static file added

---
+++ precache-manifest.369eb3cc66ec0f550a9b9fa42d41bcfb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddf58f90130e536205a7f4a029af79a5",
+    "url": "/static/media/play-button.ddf58f90.svg"
+  },
+  {
+    "revision": "1850dbca26ca670ccc6d7a4e1a0a6e48",
+    "url": "/static/media/pause-button.1850dbca.svg"
+  },
+  {
+    "revision": "26cc47e43629eeb26d5c4340bb17f733",
+    "url": "/static/media/bg.26cc47e4.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7cc575b8719b73576527",
+    "url": "/static/js/main.7cc575b8.chunk.js"
+  },
+  {
+    "revision": "5de85fdff08e74a8f6d5",
+    "url": "/static/js/1.5de85fdf.chunk.js"
+  },
+  {
+    "revision": "7cc575b8719b73576527",
+    "url": "/static/css/main.fc65ec32.chunk.css"
+  },
+  {
+    "revision": "118b2be08c86a0067cc25030cc1d0210",
+    "url": "/index.html"
+  }
+];
